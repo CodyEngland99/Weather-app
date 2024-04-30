c74fbbc445e4b3ed6bf21e88bcf0489fd3dd32c0
cody <codywengland@gmail.com>
2024-04-29 21:17:00 -0500

    FINSIHED NOT THE BEST NEED BETTER API

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
+// PULLING API AND SETTING KEY
 const apiKey = "bb3d1503a5e34683b1427979542d355e";
 const apiUrl = "https://api.weatherbit.io/v2.0/current?";
 const userCity = document.getElementById("user-city");
-const subButton = document.getElementById("submit");
 
 async function checkWeather(city) {
 	let cityPost = `&city=${city}&country=US`;
@@ -11,6 +11,21 @@ async function checkWeather(city) {
 		.then((data) => generateCards(data));
 }
 
+// PULLING API AND SETTING KEY
+
+//PULLING HTML CONTENT
+const subButton = document.getElementById("submit");
+const cityName = document.getElementById("city-name");
+const weatherCurrent = document.getElementById("weather-description");
+const tempCur = document.getElementById("cur-temp");
+const iconWeather = document.getElementById("icon-weather");
+const cloundCoverage = document.getElementById("cloud-coverage");
+const windDirection = document.getElementById("wind-direction");
+const airQlt = document.getElementById("air-quality");
+const vison = document.getElementById("vision");
+//PULLING HTML CONTENT
+
+// GETTING USER INPUT
 const userInput = () => {
 	const userCity = document.getElementById("user-city").value;
 
@@ -20,28 +35,23 @@ const userInput = () => {
 		checkWeather(userCity);
 	}
 };
+// GETTING USER INPUT
 
+// SETTING INFORMATION INTO HTML FROM API
 const generateCards = (data) => {
-	const cityName = document.getElementById("city-name");
-	const weatherCurrent = document.getElementById("weather-description");
-	const tempCur = document.getElementById("cur-temp");
-	const iconWeather = document.getElementById("icon-weather");
-	const cloundCoverage = document.getElementById("cloud-coverage");
-	const windDirection = document.getElementById("wind-direction");
-	const sunrise = document.getElementById("sunrise");
-	const sunset = document.getElementById("sunset");
-  
-
 	const celToFahr = Math.round(data.data[0].app_temp * 1.8 + 32);
 
 	cityName.innerHTML = `${data.data[0].city_name}`;
-	weatherCurrent.innerHTML = `Weather Description: ${data.data[0].weather.description}`;
-	tempCur.innerHTML = `Current Tempature: ${celToFahr}&deg;`;
-	iconWeather.src = `image/icons/icons/${data.data[0].weather.icon}.png`
-	cloundCoverage.innerHTML = `Cloud Coverage: ${data.data[0].clouds}%`
-	windDirection.innerHTML = `Wind Direction: ${data.data[0].wind_cdir}`
-	sunrise.innerHTML = `Sunrise: ${data.data[0].sunrise}`
-	sunset.innerHTML = `Sunrise: ${data.data[0].sunset}`
+	weatherCurrent.innerHTML = `${data.data[0].weather.description}`;
+	tempCur.innerHTML = `${celToFahr}&deg;`;
+	iconWeather.src = `image/icons/icons/${data.data[0].weather.icon}.png`;
+	cloundCoverage.innerHTML = `Coverage: ${data.data[0].clouds}%`;
+	airQlt.innerHTML = `Air Quality: ${data.data[0].aqi}`;
+	windDirection.innerHTML = `Wind Direction: ${data.data[0].wind_cdir}`;
+	vison.innerHTML = `Visibility: ${data.data[0].vis} Miles`;
 };
+// SETTING INFORMATION INTO HTML FROM API
 
+// EVENT LISTENER
 subButton.addEventListener("click", userInput);
+// EVENT LISTENER
